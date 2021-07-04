044031d37e4001527f06e95a0df544ee8b9831c1
Lukim99 <86387262+Lukim99@users.noreply.github.com>
2021-07-04 22:28:01 +0900

    Update login.js

--- login.js
+++ login.js
@@ -1,13 +1,7 @@
 function check() {
-  var id = document.getElementById("id").innerText;
-  var password = document.getElementById("pass").innerText;
-  if (id == "") {
-    alert("아이디를 입력해주세요.");
-  }
-  else if (password == "") {
-    alert("비밀번호를 입력해주세요.");
-  }
-  else if (id != "Lukim9" || password != "yanga0800") {
+  var id = document.getElementById("id").value;
+  var password = document.getElementById("pass").value;
+  if (id != "Lukim9" || password != "yanga0800") {
     alert("아이디 또는 비밀번호가 잘못되었습니다.");
   }
   else {
