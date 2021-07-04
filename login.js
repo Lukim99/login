23e33a606f78c0ec4767626b1b2406441552e059
Lukim99 <86387262+Lukim99@users.noreply.github.com>
2021-07-04 22:29:03 +0900

    Update login.js

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ function check() {
   var id = document.getElementById("id").value;
   var password = document.getElementById("pass").value;
   if (id != "Lukim9" || password != "yanga0800") {
-    alert("아이디 또는 비밀번호가 잘못되었습니다.");
+    alert("아이디 또는 비밀번호가 잘못되었습니다." + id + ":" + password);
   }
   else {
     alert("로그인에 성공하였습니다.");
