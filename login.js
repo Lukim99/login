0e51aa65606965125a20864417996d1befdd28d2
Lukim99 <86387262+Lukim99@users.noreply.github.com>
2021-07-04 22:10:26 +0900

    Update login.js

--- login.js
+++ login.js
@@ -1,15 +1,14 @@
-function check(f) {
-  if (f.id.value == "") {
+function check() {
+  var id = document.getElementById("id").innerHTML;
+  var password = document.getElementById("pass").innerHTML;
+  if (id == "") {
     alert("아이디를 입력해주세요.");
-    location.href = "https://login.ykm-dev.site";
   }
-  else if (f.pass.value == "") {
+  else if (password == "") {
     alert("비밀번호를 입력해주세요.");
-    location.href = "https://login.ykm-dev.site";
   }
-  else if (f.id.value != "Lukim9" || f.id.value != "yanga0800") {
+  else if (id != "Lukim9" || password != "yanga0800") {
     alert("아이디 또는 비밀번호가 잘못되었습니다.");
-    location.href = "https://login.ykm-dev.site";
   }
   else {
     alert("로그인에 성공하였습니다.");
